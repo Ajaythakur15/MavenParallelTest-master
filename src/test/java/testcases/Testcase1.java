181652dad67e3129d94690e2c9255e9738a67682
Ajaythakur15 <tech.ajaythakur@hotmail.com>
2025-01-30 15:56:01 +0530

    4th commit

--- src/test/java/testcases/Testcase1.java
+++ src/test/java/testcases/Testcase1.java
@@ -2,46 +2,37 @@ package testcases;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
 public class Testcase1 {
 
-	@Test
-	public void Reg_Test1() throws InterruptedException {
-		//New changes from GitHub
-		WebDriverManager.chromedriver().setup();
-		//New changes from IntelliJ
-		WebDriver driver=new ChromeDriver();
-		
-		driver.get("https://www.google.com/");	
-		
-		System.out.println("==> Regression Test case 1 has run successfully...");
-		
-		driver.close();
-				
-	}
-	
-	@Test
-	public void chromeBrowserTest2() throws InterruptedException {
-		
-		WebDriverManager.chromedriver().setup();
-		
-		WebDriver driver=new ChromeDriver();
-		
-		driver.get("https://www.gmail.com/");	
-		
-		
-		driver.close();
-				
-	}
-	
-	
-	
-	
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void Reg_Test1() {
+        driver.get("https://www.google.com/");
+        System.out.println("==> Regression Test case 1 has run successfully...");
+    }
+
+    @Test
+    public void chromeBrowserTest2() {
+        driver.get("https://www.gmail.com/");
+    }
+
+    @AfterMethod
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();  // Ensures complete browser shutdown
+        }
+    }
 }
--- src/test/java/testcases/Testcase2.java
+++ src/test/java/testcases/Testcase2.java
@@ -1,36 +1,38 @@
 package testcases;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Testcase2 {
 
-	@Test
-	public void firefoxBrowserTest1() throws InterruptedException {
-		
-		WebDriverManager.firefoxdriver().setup();
-		
-		WebDriver driver=new FirefoxDriver();
-		
-		driver.get("https://www.google.com/");	
-		
-		driver.close();
-				
-	}
-	
-	@Test
-	public void firefoxBrowserTest2() throws InterruptedException {
-		
-		WebDriverManager.firefoxdriver().setup();
-		
-		WebDriver driver=new FirefoxDriver();
-		
-		driver.get("https://www.gmail.com/");	
-	
-		driver.close();
-				
-	}
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void Fun_Test1() {
+        driver.get("https://www.google.com/");
+        System.out.println("==> Firefox Test 1 executed.");
+    }
+
+    @Test
+    public void chromeBrowserTest2() {
+        driver.get("https://www.gmail.com/");
+        System.out.println("==> Firefox Test 2 executed.");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
